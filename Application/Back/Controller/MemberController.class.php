cce95a5146004a37a2ee202cb3d0f1796c29afff
jkloozx <jkloozx@gmail.com>
2017-02-12 22:42:51 +0800

    测试demo

--- Application/Back/Controller/MemberController.class.php
+++ Application/Back/Controller/MemberController.class.php
@@ -5,11 +5,9 @@ namespace Back\Controller;
 use Think\Controller;
 
 
-class MemberController extends Controller
-{
+class MemberController extends Controller {
 
-    public function listAction()
-    {
+    public function listAction() {
 
         $this->assign('list', M('Member')->select());
 
@@ -17,9 +15,7 @@ class MemberController extends Controller
     }
 
 
-
-    public function multiAction()
-    {
+    public function multiAction() {
         // 获取操作类型
         $type = I('post.type', null);
         if (is_null($type)) {
@@ -29,21 +25,21 @@ class MemberController extends Controller
         // 根据操作类型进行不同的操作
         switch ($type) {
 
-            case 'email': 
+            case 'email':
                 // 不做: 找到每个需要发送的邮件的emial地址, 对其发送邮件即可
-                
+
                 // 将任务加入队列
-                
+
                 $redis = new \Redis;
                 $redis->connect('192.168.118.128', '6379');
                 // 将需要发送邮件的会员的ID加入队列, 任务入队列
-                foreach(I('post.selected', []) as $member_id) {
-                    $redis->lpush('memberEmailQueue', $member_id); 
+                foreach (I('post.selected', []) as $member_id) {
+                    $redis->lpush('memberEmailQueue', $member_id);
                 }
                 $redis->close();
-                $this->success('邮件队列加入成功, 需要耐心等待发送完成通知', U('list'), 1); 
-                
-            break;
+                $this->success('邮件队列加入成功, 需要耐心等待发送完成通知', U('list'), 1);
+
+                break;
         }
     }
 }
--- Application/Back/Controller/WeixinController.class.php
+++ Application/Back/Controller/WeixinController.class.php
@@ -9,49 +9,26 @@ class WeixinController extends Controller {
 
 
     public function replyAction() {
-//        $options = array(
-//            'token'=>'jkloozx', //填写你设定的key
-//            'encodingaeskey'=>'Bgmr5zO13SkH0zy8QpHQfuJoAgStUAzORusqUzuempp', //填写加密用的EncodingAESKey
-//            'appid'=>'wx9312e7bdbaa7b825', //填写高级调用功能的app id, 请在微信开发模式后台查询
-//            'appsecret'=>'6028a7cbd83a463528148571f4700afd' //填写高级调用功能的密钥
-//        );
-//        $wx = new \Org\Wechat\wechat($options);
-//        $model_reply = M('wx_reply');
-//
-//        $wx->valid();
-//        $type = $wx->getRev()->getRevType();
-//        switch($type) {
-//            case Wechat::MSGTYPE_TEXT:
-//                $key = $wx->getRev()->getRevContent();
-//                if ($content = $model_reply -> getByReply_key($key)){
-////                    $wx->text($content)->reply();
-//                    $wx->text("hello world")->reply();
-//                }else{
-//                    $wx->text('你说什么，我听不清楚~')->reply();
-//                }
-//                exit;
-//                break;
-//            case Wechat::MSGTYPE_EVENT:
-//                break;
-//            case Wechat::MSGTYPE_IMAGE:
-//                break;
-//            default:
-//                $wx->text("help info")->reply();
-//        }
-        function logdebug($text){
-            file_put_contents('../data/log.txt',$text."\n",FILE_APPEND);
-        };
         $options = array(
             'token'=>'jkloozx', //填写你设定的key
-            'debug'=>true,
-            'logcallback'=>'logdebug'
+            'encodingaeskey'=>'Bgmr5zO13SkH0zy8QpHQfuJoAgStUAzORusqUzuempp', //填写加密用的EncodingAESKey
+            'appid'=>'wx9312e7bdbaa7b825', //填写高级调用功能的app id, 请在微信开发模式后台查询
+            'appsecret'=>'6028a7cbd83a463528148571f4700afd' //填写高级调用功能的密钥
         );
-        $weObj = new \Org\Wechat\Wechat($options);
-        $weObj->valid();
-        $type = $weObj->getRev()->getRevType();
+        $wx = new \Org\Wechat\Wechat($options);
+        $model_reply = M('wx_reply');
+
+        $wx->valid();
+        $type = $wx->getRev()->getRevType();
         switch($type) {
             case Wechat::MSGTYPE_TEXT:
-                $weObj->text("hello, I'm wechat")->reply();
+                $key = $wx->getRev()->getRevContent();
+                if ($content = $model_reply -> getByReply_key($key)){
+//                    $wx->text($content)->reply();
+                    $wx->text("hello world")->reply();
+                }else{
+                    $wx->text('你说什么，我听不清楚~')->reply();
+                }
                 exit;
                 break;
             case Wechat::MSGTYPE_EVENT:
@@ -59,8 +36,31 @@ class WeixinController extends Controller {
             case Wechat::MSGTYPE_IMAGE:
                 break;
             default:
-                $weObj->text("help info")->reply();
+                $wx->text("help info")->reply();
         }
+//        function logdebug($text){
+//            file_put_contents('../data/log.txt',$text."\n",FILE_APPEND);
+//        };
+//        $options = array(
+//            'token'=>'jkloozx', //填写你设定的key
+//            'debug'=>true,
+//            'logcallback'=>'logdebug'
+//        );
+//        $weObj = new \Org\Wechat\Wechat($options);
+//        $weObj->valid();
+//        $type = $weObj->getRev()->getRevType();
+//        switch($type) {
+//            case Wechat::MSGTYPE_TEXT:
+//                $weObj->text("hello, I'm wechat")->reply();
+//                exit;
+//                break;
+//            case Wechat::MSGTYPE_EVENT:
+//                break;
+//            case Wechat::MSGTYPE_IMAGE:
+//                break;
+//            default:
+//                $weObj->text("help info")->reply();
+//        }
 
 
 
